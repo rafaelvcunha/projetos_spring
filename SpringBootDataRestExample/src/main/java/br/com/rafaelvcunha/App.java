1bec58b00b763baca2f72df81deaf7831f83d912
rafaelvcunha <rafaelvcunha@gmail.com>
2017-04-12 22:08:00 -0300

    finalizando o repository e criado o executor

--- SpringBootDataRestExample/src/main/java/br/com/rafaelvcunha/App.java
+++ SpringBootDataRestExample/src/main/java/br/com/rafaelvcunha/App.java
@@ -1,13 +1,20 @@
 package br.com.rafaelvcunha;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"br.com.rafaelvcunha.server.model"})
+@EnableJpaRepositories(basePackages = {"br.com.rafaelvcunha.server.repository"})
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+    	
+    	SpringApplication.run(App.class, args);
+    	
     }
+
+    
 }
--- SpringBootDataRestExample/src/main/java/br/com/rafaelvcunha/server/repository/ReportRepository.java
+++ SpringBootDataRestExample/src/main/java/br/com/rafaelvcunha/server/repository/ReportRepository.java
@@ -1,12 +1,35 @@
 package br.com.rafaelvcunha.server.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import br.com.rafaelvcunha.server.model.Report;
 
 @RepositoryRestResource(collectionResourceRel = "report", path = "reports")
-
 public interface ReportRepository extends PagingAndSortingRepository<Report, Long> {
 
+   
+    /**
+     * Método que retorna a linha fazendo a busca com o id passado como parâmetro.
+     * 
+     * @param id
+     * @return Report do id passado como parâmetro.
+     */   
+    @Query("SELECT * FROM RAW_REPORT r where r.id = :id") 
+    Report findLinhaById(@Param("id") Long id);
+    
+    /**
+     * Método que retorna uma lista de staffName fazendo a busca pelo staffName passado como parâmetro e ordenando os 
+     * pelo staffName.
+     *  
+     * @param staffName
+     * @return lista de staffName
+     */
+    List<Report> findByStaffNameOrderByStaffName(@Param("staffName") String staffName);
+
+	
 }
