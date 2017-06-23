28835d107a4640875fdbff02e2725c7f1cee9c72
rafaelvcunha <rafaelvcunha@gmail.com>
2017-06-23 10:19:07 -0300

    Implementado validador de produto

--- CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,8 +2,13 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("/produtos")
@@ -19,6 +25,11 @@ public class ProdutosController {
 	
 	@Autowired
 	private ProdutoDAO produtoDAO;
+	
+	@InitBinder
+	public void InitBinder(WebDataBinder webDataBinder){
+		webDataBinder.addValidators( new ProdutoValidation());
+	}
 	
 	@RequestMapping("/form")
 	public ModelAndView form(){
@@ -30,13 +41,17 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-    public String gravar(Produto produto, RedirectAttributes redirectAttributes) {
+    public ModelAndView gravar(@Valid Produto produto, BindingResult bindingResult , RedirectAttributes redirectAttributes) {
 		
-        System.out.println(produto);
+        if(bindingResult.hasErrors()){
+        	//return form();
+        	return new ModelAndView("redirect:produtos/form");
+        }
+		
         produtoDAO.gravar(produto);
         redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
         
-        return "redirect:/produtos";
+        return new ModelAndView("redirect:produtos");
     }
 	
 	@RequestMapping(method = RequestMethod.GET)
---
+++ CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/validation/ProdutoValidation.java
@@ -0,0 +1,30 @@
+package br.com.casadocodigo.loja.validation;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import br.com.casadocodigo.loja.models.Produto;
+
+
+public class ProdutoValidation implements Validator {
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Produto.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "titulo", "field.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "descricao", "field.required");
+		
+		Produto produto = (Produto) target;
+		if(produto.getPaginas() <= 0){
+			errors.rejectValue("paginas", "field.required");
+		}
+		
+	}
+
+}
