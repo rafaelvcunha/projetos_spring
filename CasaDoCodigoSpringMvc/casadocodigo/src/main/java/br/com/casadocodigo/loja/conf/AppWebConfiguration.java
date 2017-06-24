e44dabe30983226c751430c7533ba7ccb8a1b059
rafaelvcunha <rafaelvcunha@gmail.com>
2017-06-24 16:47:05 -0300

    Apresentando detalhes do produto

--- CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -10,7 +10,9 @@ import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import br.com.casadocodigo.loja.controllers.HomeController;
@@ -19,7 +21,7 @@ import br.com.casadocodigo.loja.infra.FileSaver;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses={HomeController.class, ProdutoDAO.class, FileSaver.class})
-public class AppWebConfiguration {
+public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 	
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver(){
@@ -59,5 +61,10 @@ public class AppWebConfiguration {
 	    return new StandardServletMultipartResolver();
 	}
 	
+	
+	 @Override
+	 public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+	        configurer.enable();
+	    }
 
 }
--- CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -22,7 +22,14 @@ public class ProdutoDAO {
 	}
 	
 	public List<Produto> listar(){
-		return manager.createQuery("Select p from Produto p", Produto.class).getResultList();
+		return manager.createQuery("select p from Produto p", Produto.class).getResultList();
+	}
+
+	public Produto find(Integer id) {
+		return manager.createQuery("select distinct(p) from Produto p "
+				+ "join fetch p.precos where id = :id", Produto.class)
+				.setParameter("id", id)
+				.getSingleResult();
 	}
 	
 
