e4178b61c244fb5fafcd32237b0d058587acbb68
rafaelvcunha <rafaelvcunha@gmail.com>
2017-07-13 21:47:55 -0300

    continuando implementacao de finalizar compra

--- CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -1,5 +1,21 @@
 package br.com.casadocodigo.loja.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.models.CarrinhoCompras;
+
+@Controller
+@RequestMapping("/pagamento")
 public class PagamentoController {
 
+	@Autowired
+	CarrinhoCompras carrinho;
+	
+	public ModelAndView finalizar(){
+		System.out.println(carrinho.getTotal());
+		return new ModelAndView("redirect:/produtos");
+	}
 }
