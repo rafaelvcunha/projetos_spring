4488592237ea910da3ede80d16d73b3726367491
rafaelvcunha <rafaelvcunha@gmail.com>
2017-07-20 14:25:39 -0300

    Habilitando Spring Security

--- CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -6,7 +6,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
--- CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMvc.java
+++ CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMvc.java
@@ -11,12 +11,12 @@ public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletI
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return null;
+		return new Class[]{SecurityConfiguration.class, AppWebConfiguration.class, JPAConfiguration.class};
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class[]{AppWebConfiguration.class, JPAConfiguration.class};
+		return new Class[]{};
 	}
 
 	@Override
