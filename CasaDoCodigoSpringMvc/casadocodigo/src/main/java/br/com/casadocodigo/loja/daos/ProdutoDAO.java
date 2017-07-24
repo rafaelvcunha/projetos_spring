4bb7281b8d76ae70e5f65baa3829b4a026e15af8
rafaelvcunha <rafaelvcunha@gmail.com>
2017-07-24 15:54:44 -0300

    Testando a aplicacao

--- CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ CasaDoCodigoSpringMvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -1,14 +1,17 @@
 package br.com.casadocodigo.loja.daos;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Repository
 @Transactional
@@ -31,7 +34,14 @@ public class ProdutoDAO {
 				.getSingleResult();
 	}
 	
-	
+	public BigDecimal somaPrecosPorTipoPreco(TipoPreco tipoPreco){
+		TypedQuery<BigDecimal> query = manager
+				.createQuery("select sum(preco.valor) from Produto p"
+						+ "join p.precos preco where preco.tipo = :tipoPreco", BigDecimal.class);
+		query.setParameter("tipoPreco", tipoPreco);
+		
+		return query.getSingleResult();
+	}
 	
 
 }
